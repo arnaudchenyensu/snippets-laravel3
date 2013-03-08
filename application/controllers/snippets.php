7ed1fe4ebc112486aa7beccfaf8d4f62233b6fbe
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2013-03-08 08:29:15 +0400

    Cleaning up

--- application/controllers/snippets.php
+++ application/controllers/snippets.php
@@ -4,11 +4,6 @@ class Snippets_Controller extends Base_Controller {
 
 	public $restful = true;
 
-	public function get_index()
-    {
-
-    }
-
 	public function post_create()
     {
         // Validate
@@ -35,25 +30,10 @@ class Snippets_Controller extends Base_Controller {
         return $this->get_new($snippet->snippet);
     }
 
-	public function get_edit()
-    {
-
-    }
-
 	public function get_new($default_snippet = '')
     {
         return View::make('snippet.new')
             ->with('snippet', $default_snippet);
-    }
-
-	public function put_update()
-    {
-
-    }
-
-	public function delete_destroy()
-    {
-
     }
 
 }
